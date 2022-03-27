7b9a6bc67f37af7fb94bc9a7744d4078a279d57e
Akshay Verma <topensite@gmail.com>
2022-03-27 12:11:43 +0530

    6th problem of Implementation submitted

---
+++ Implementation and Constructive/6.py
@@ -0,0 +1,21 @@
+def solve(n):
+    result=[];power=0
+    while n!=0:
+        digit=n%10
+        # print("number = ",n)
+        # print("digit = ",digit)
+        # print("power = ",power)
+        if digit==0:
+            power+=1
+        else:
+            result.append(digit*pow(10,power))
+            power+=1
+        # print("result = ",result)
+        n=n//10
+    return result
+t=int(input())
+for i in range(t):
+    query=int(input())
+    ans=solve(query)
+    print(len(ans))
+    print(" ".join(list(map(str,ans))))

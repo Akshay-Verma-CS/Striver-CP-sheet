1a73147a3ae2e200358fbbffcb32824f8e4c3791
Akshay Verma <topensite@gmail.com>
2022-04-16 19:05:58 +0530

    116 - Tram

---
+++ Implementation and Constructive/13.py
@@ -0,0 +1,9 @@
+n = int(input())
+maxx=0
+current_cap = 0
+for i in range(n):
+    x = list(map(int,input().split()))
+    current_cap=(current_cap - x[0]) + x[1]
+    maxx=max(current_cap,maxx)
+print(maxx)
+    
